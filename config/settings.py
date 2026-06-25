23b9809acdf6a167c407bcd3e33a2d2f408583ee
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-06-24 23:00:34 -0500

    v2.6.0: Arsenal-Metatron Edition

--- config/settings.py
+++ config/settings.py
@@ -3,8 +3,8 @@
 #  Edita este archivo para personalizar Visor
 # ─────────────────────────────────────────────
 
-VERSION      = "2.5.0"
-APP_NAME     = "Visor v2.5 Metatron Edition"
+VERSION      = "2.6.0"
+APP_NAME     = "Visor v2.6 Arsenal-Metatron Edition"
 ORGANIZATION = "Jasol Group"
 AUTOR        = "Ing. Jeisson Alberto Sarmiento"
 UBICACION    = "Saravena, Arauca, Colombia"

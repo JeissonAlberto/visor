23ba6bc7fe97096bcaada902de9979be211214e4
JeissonAlberto <126131566+JeissonAlberto@users.noreply.github.com>
2026-07-15 08:32:20 -0500

    fix: visor.py - saltos de linea literales corregidos

--- visor.py
+++ visor.py
@@ -1,1 +1,6 @@
-import sys\nimport os\nsys.path.insert(0, os.path.dirname(__file__))\nfrom main import main\nif __name__ == \"__main__\":\n    main()
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+from main import main
+if __name__ == "__main__":
+    main()
